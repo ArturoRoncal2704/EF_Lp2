da77e8abf4f069547a9d5486b5142ae9ba884658
ArturoRoncal2704 <roncalhermenegildo@gmail.com>
2024-10-23 12:35:37 -0500

    Entity y repository del usuario

---
+++ src/main/java/com/example/demo/model/UsuarioEntity.java
@@ -0,0 +1,53 @@
+package com.example.demo.model;
+
+
+
+
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+
+@Entity
+@Table(name="tb_usuario", uniqueConstraints = @UniqueConstraint(columnNames = "dniUsuario"))
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UsuarioEntity {
+
+	@Id
+	@Column(name = "email", nullable = false, length =60)
+	private String email;
+	
+	@Column(name = "nombre", length = 60, nullable = false)
+	private String nombre;
+	
+	@Column(name = "password", nullable = false)
+	private String password;
+	
+	@Column(name = "apellido", length = 60, nullable = false)
+	private String apellido;
+	
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	@Column(name="fecha_nacimiento")
+	private Date fechaNaci;
+	
+	@Column(name = "url_imagen")
+	private String urlImagen;
+	
+	
+}
