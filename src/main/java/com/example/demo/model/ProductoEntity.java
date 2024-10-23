3d2e33d659511a02d362f3d0b9bbc053757a1a54
ArturoRoncal2704 <roncalhermenegildo@gmail.com>
2024-10-23 14:24:12 -0500

    Entidades del CRUD añadidas

---
+++ src/main/java/com/example/demo/model/ProductoEntity.java
@@ -0,0 +1,39 @@
+package com.example.demo.model;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="tb_producto")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductoEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_producto")
+	private Integer idProducto;
+	
+	@Column(name = "nombre_producto",nullable = false, length = 45 )
+	private String nombreProducto;
+	
+	@Column(name = "precio",nullable = false, length = 45)
+	private Double precio;
+	
+	@ManyToOne
+	@JoinColumn(name ="categoria_id", nullable = false)
+	private CategoríaEnrtity categoriaEntity;
+}
